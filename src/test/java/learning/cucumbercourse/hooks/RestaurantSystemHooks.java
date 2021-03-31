f2fbcd730d2a5b8a09c038675ef038f41088bd8f
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-03-31 16:02:28 +0300

    Added tagged cucumber hooks

--- src/test/java/learning/cucumbercourse/hooks/RestaurantSystemHooks.java
+++ src/test/java/learning/cucumbercourse/hooks/RestaurantSystemHooks.java
@@ -5,13 +5,13 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
 public class RestaurantSystemHooks {
-	@Before
-	public void BeforeDisplayMessage(Scenario scenario) {
-		System.out.println("Before: " + scenario.getName());
-	}
+    @Before("@SmokeTest")
+    public void BeforeDisplayMessage(Scenario scenario) {
+        System.out.println("Before: " + scenario.getName());
+    }
 
-	@After
-	public void AfterDisplayMessage(Scenario scenario) {
-		System.out.println("After: " + scenario.getName());
-	}
+    @After("@SmokeTest")
+    public void AfterDisplayMessage(Scenario scenario) {
+        System.out.println("After: " + scenario.getName());
+    }
 }
