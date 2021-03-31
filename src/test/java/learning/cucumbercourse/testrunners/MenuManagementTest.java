2225a83ccb1355a280ca35dda7259d2bd010824e
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-03-31 15:03:42 +0300

    Tested run maven test with tags

--- src/test/java/learning/cucumbercourse/testrunners/MenuManagementTest.java
+++ src/test/java/learning/cucumbercourse/testrunners/MenuManagementTest.java
@@ -8,8 +8,10 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/java/learning/cucumbercourse/features/MenuManagement.feature"},
         glue = {"learning.cucumbercourse.stepdefinitions"},
-        tags = "@SmokeTest or (@NightlyBuildTest and @RegularTest)",  // "not @NightlyBuildTest"
+     //   tags = "@SmokeTest or (@NightlyBuildTest and @RegularTest)",  // "not @NightlyBuildTest"
         plugin = {"pretty"}
+
+        //run maven test with tags: mvn test -Dcucumber.filter.tags="@SmokeTest or @NightlyBuildTest"
 )
 public class MenuManagementTest {
 }
