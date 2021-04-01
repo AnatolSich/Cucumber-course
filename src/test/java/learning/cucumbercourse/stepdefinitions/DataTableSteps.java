7458a2cce35b486fa1b80ecc718912fbec5831f4
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-04-01 16:53:02 +0300

    Adde data table with listOfString parsing

---
+++ src/test/java/learning/cucumbercourse/stepdefinitions/DataTableSteps.java
@@ -0,0 +1,46 @@
+package learning.cucumbercourse.stepdefinitions;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+
+public class DataTableSteps {
+	@Given("I placed an order for the following items")
+	public void i_placed_an_order_for_the_following_items(io.cucumber.datatable.DataTable dataTable) {
+	  // Write code here that turns the phrase above into concrete actions
+	  // For automation transformation, change DataTable to one of 
+	  // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	  // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	  // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	  //
+	  // For other transformations you can register a DataTableType.
+
+		/*String dishName = dataTable.cell(0,0);
+		String amount = dataTable.cell(0,1);
+		String price = dataTable.cell(0,2);
+		System.out.println(dishName);
+		System.out.println(amount);
+		System.out.println(price);*/
+		List<String> billData = dataTable.asList();
+	//	List<String> billData = dataTable.row(0);
+	//	List<String> billData = dataTable.transpose().asList(String.class);
+
+		for (String billItem: billData) {
+			System.out.println(billItem);
+		}
+	}
+
+
+	@When("I generate the bill")
+	public void i_generate_the_bill() {
+		// Write code here that turns the phrase above into concrete actions
+	   }
+
+	@Then("a bill for ${int} should be generated")
+	public void a_bill_for_$_should_be_generated(Integer int1) {
+	  	// Write code here that turns the phrase above into concrete actions
+	}
+}
--- src/test/java/learning/cucumbercourse/testrunners/MenuManagementTest.java
+++ src/test/java/learning/cucumbercourse/testrunners/MenuManagementTest.java
@@ -6,15 +6,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/java/learning/cucumbercourse/features/MenuManagement.feature"},
+        features = {"src/test/java/learning/cucumbercourse/features/"},
         glue = {"learning.cucumbercourse.stepdefinitions"},
         //tags = "@SmokeTest or (@NightlyBuildTest and @RegularTest)",  // "not @NightlyBuildTest"
         plugin = {"pretty",
-                "html:target/SystemTestReports/html",
-                "json:target/SystemTestReports/json/report.json",
-                "junit:target/SystemTestReports/junit/report.xml"},
+      //          "html:target/SystemTestReports/html",
+       //         "json:target/SystemTestReports/json/report.json",
+       //         "junit:target/SystemTestReports/junit/report.xml"
+        },
+        tags = "@ListOfStrings",
         dryRun = false,
-        monochrome = true
+        monochrome = false
 )
 
 //run maven test with tags: mvn test -Dcucumber.filter.tags="@SmokeTest or @NightlyBuildTest"
