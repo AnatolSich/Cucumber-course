a1dc4bc96d0eaf8c69bfbddc9cb86fce9cbe6e82
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-04-01 22:42:47 +0300

    Added web browser automation testing to scenario outlines

--- src/test/java/learning/cucumbercourse/stepdefinitions/ScenarioOutlineSteps.java
+++ src/test/java/learning/cucumbercourse/stepdefinitions/ScenarioOutlineSteps.java
@@ -4,6 +4,9 @@ package learning.cucumbercourse.stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import learning.cucumbercourse.BillCalculationHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertEquals;
 
@@ -31,6 +34,36 @@ public class ScenarioOutlineSteps {
         System.out.println("Tax Rate: " + taxRate);
     }
 
+    private void invokeWebPage(Double expectedValue) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:8080/BasicServletSite/");
+
+        //ids of web elements were gotten from index page->element->investigate
+        WebElement billAmountTextBox = driver.findElement(By.id("billamount"));
+        WebElement taxRateTextBox = driver.findElement(By.id("taxrate"));
+        WebElement button = driver.findElement(By.id("mybutton"));
+
+        //sending info to elements
+        billAmountTextBox.sendKeys(Integer.toString(InitialBillAmount));
+        taxRateTextBox.sendKeys(Double.toString(TaxRate));
+
+        button.click();
+
+        //find all h1 elements on result page
+        WebElement Header1Element = driver.findElementByXPath("//h1");
+
+        System.out.println(Header1Element.getText());
+
+        //check if value of h1 element is as expected
+        boolean matched = Header1Element.getText().contains("Final Bill Amount is: $" + expectedValue.toString());
+
+        System.out.println(matched);
+
+      //  driver.quit();
+    }
 
     @Then("final bill amount calculated is {double}")
     public void final_bill_amount_calculate_is(Double expectedValue) {
@@ -40,6 +73,7 @@ public class ScenarioOutlineSteps {
         System.out.println("Calculated Value: " + SystemCalcValue);
         System.out.println();
         assertEquals(expectedValue, SystemCalcValue, 0.0);
+        invokeWebPage(expectedValue);
     }
 
 }
--- src/test/java/learning/cucumbercourse/testrunners/BillScenarioOutlinesTestRunner.java
+++ src/test/java/learning/cucumbercourse/testrunners/BillScenarioOutlinesTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/learning/cucumbercourse/features/ScenarioOutlineExample.feature"},
-        glue = {"learning.cucumbercourse.stepdefinitions.ScenarioOutlineSteps"},
+        glue = {"learning.cucumbercourse.stepdefinitions"},
         plugin = {"pretty"},
         tags = "@ScenarioOutlineExample",
         dryRun = false,
